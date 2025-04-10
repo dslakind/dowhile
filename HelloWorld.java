914f9696ab7e14eda4531485cdfa3443e8604fce
David S Lakind <40665394+dslakind@users.noreply.github.com>
2025-04-10 18:20:01 -0400

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Class to help users test the initial setup.
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# AP CSA Codespace
+## Mr. Lakind
+
+We will use this codespace to run PLTW assignments and projects.
